991699583d123fd8ffc17827d4ed2eb1f6869fbd
Simon Fay <simonfay@Simon-Fay-MacBookPro-Work.local>
2026-07-16 16:48:44 +0100

    Update example to use Configurable Delay)

--- PluginProcessorConfig.h
+++ PluginProcessorConfig.h
@@ -10,12 +10,33 @@
 
 
 #include <sjf/helpers/sjf_DummyProcessor.h>
+
+#include "sjf/helpers/sjf_OversamplingWrapper.h"
+#include "sjf/processors/sjf_Delay.h"
+
 namespace sjf::plugin_processor_config
 {
     struct Config
     {
+        using Delay = sjf::dsp::Delay<  dsp::delay_config::TempoSync,
+                                        dsp::delay_config::Filter,
+                                        dsp::delay_config::Detune,
+                                        dsp::delay_config::Saturation,
+                                        dsp::delay_config::PingPong,
+                                        dsp::delay_config::Modulation,
+                                        dsp::delay_config::Link,
+                                        dsp::delay_config::LFOTempoSync>;
+
         // Simply change this alias target to swap out the active core engine
-        using Processor = sjf::helpers::Chunker<sjf::helpers::ProcessorSequence<sjf::helpers::BypassWrapper<sjf::DummyProcessor>>>;
+        using Processor = sjf::helpers::ChunkedWrapper  <
+                                sjf::helpers::ProcessorSequence <
+                                        sjf::helpers::BypassWrapper <
+                                                sjf::helpers::OversamplingWrapper<Delay>,
+                                                sjf::helpers::bypass_wrapper_config::Bypass,
+                                                sjf::helpers::bypass_wrapper_config::Mix
+                                                                    >
+                                                                >
+                                                        >;
 
         template<typename Processor>
         static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout(Processor& processor)
