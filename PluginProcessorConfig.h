b88aa4c8c7a53e336fee5366219eb0fa21a4f32c
Simon Fay <simonfay@Simon-Fay-MacBookPro-Work.local>
2026-07-17 12:19:08 +0100

    Update namespace

--- PluginProcessorConfig.h
+++ PluginProcessorConfig.h
@@ -19,11 +19,11 @@ namespace sjf::plugin_processor_config
     struct Config
     {
         using LFO = sjf::dsp::oscillators::lfo::LFO<dsp::oscillators::lfo::DefaultWaveformProvider,
-                                                    dsp::oscillators::lfo::config::TempoSync,
-                                                    dsp::oscillators::lfo::config::Invert,
-                                                    dsp::oscillators::lfo::config::PhaseOffset,
-                                                    dsp::oscillators::lfo::config::Smooth,
-                                                    dsp::oscillators::lfo::config::Depth>;
+                                                    dsp::oscillators::lfo::lfo_config::TempoSync,
+                                                    dsp::oscillators::lfo::lfo_config::Invert,
+                                                    dsp::oscillators::lfo::lfo_config::PhaseOffset,
+                                                    dsp::oscillators::lfo::lfo_config::Smooth,
+                                                    dsp::oscillators::lfo::lfo_config::Depth>;
 
         using Saturator = sjf::dsp::waveshaper::WaveshaperTypeProvider  <   dsp::waveshaper::SoftClip,
                                                                             dsp::waveshaper::HardClip,
@@ -34,12 +34,14 @@ namespace sjf::plugin_processor_config
 
         using Delay = sjf::dsp::Delay<  LFO,
                                         Saturator,
+                                        // dsp::delay_config::Mono,
+                                        dsp::delay_config::Feedback,
+                                        dsp::delay_config::Offset,
                                         dsp::delay_config::TempoSync,
                                         dsp::delay_config::Filter,
                                         dsp::delay_config::Detune,
                                         dsp::delay_config::PingPong,
-                                        dsp::delay_config::Link,
-                                        dsp::delay_config::LFOTempoSync>;
+                                        dsp::delay_config::Link>;
 
         // Simply change this alias target to swap out the active core engine
         using Processor = sjf::helpers::ChunkedWrapper  <
