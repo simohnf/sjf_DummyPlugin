c41a3f279ff366b8a29e841733325736df83c009
Simon Fay <simonfay@Simon-Fay-MacBookPro-Work.local>
2026-09-02 17:08:05 +0100

    Switch order of wrappers so oversampling is around bypassDecorator

--- PluginProcessorConfig.h
+++ PluginProcessorConfig.h
@@ -83,7 +83,7 @@ namespace sjf::plugin_processor_config
 
 
         // using Seq = sjf::helpers::BypassWrapper <Sequence,Bypass,Mix>;
-        using Seq = sjf::helpers::BypassWrapper <sjf::helpers::OversamplingWrapper<Sequence>,Bypass,Mix>;
+        using Seq = sjf::helpers::OversamplingWrapper<sjf::helpers::BypassWrapper <Sequence,Bypass,Mix>>;
 
         using Limit = sjf::helpers::BypassWrapper <sjf::dsp::Limiter,Bypass>;
 
